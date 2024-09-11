1ead6ed8c8e9a3e9a728ae1ba17cf28575a978b6
fabri <fguespe@gmail.com>
2024-09-11 15:03:23 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import { run, HandlerContext } from "@xmtp/message-kit";
 
 run(async (context: HandlerContext) => {
-  // Get the message and the address from the sender
-  const { content, sender } = context.message;
-  console.log(content, sender);
   // To reply, just call `reply` on the HandlerContext.
-  await context.reply(`gm`);
+  await context.send(`gm`);
+  //await context.reply(`gm2`);
 });
