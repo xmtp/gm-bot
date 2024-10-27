64008f4d0a1f896f3da33e56b1c79586eac9f620
fabri <fguespe@gmail.com>
2024-10-27 15:23:44 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { run, HandlerContext } from "@xmtp/message-kit";
 
-run(async (context: HandlerContext) => {
-  // To reply, just call `reply` on the HandlerContext.
-  await context.send(`gm`);
-  //await context.reply(`gm2`);
-});
+run(
+  async (context: HandlerContext) => {
+    // To reply, just call `reply` on the HandlerContext.
+    await context.send(`gm`);
+  },
+  {
+    client: {
+      logging: process.env.NODE_ENV === "production" ? "debug" : "off",
+    },
+  }
+);
