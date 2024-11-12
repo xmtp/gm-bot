e420314fc5168e1b56a90222b4355c2bf2608469
fabri <fguespe@gmail.com>
2024-11-12 16:57:42 +0700

    logging off

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,5 @@
 import { run, HandlerContext } from "@xmtp/message-kit";
 
-run(
-  async (context: HandlerContext) => {
-    // To reply, just call `reply` on the HandlerContext.
-    await context.send(`gm`);
-  },
-  {
-    client: {
-      logging: process.env.NODE_ENV === "production" ? "debug" : "off",
-    },
-  }
-);
+run(async (context: HandlerContext) => {
+  await context.send(`gm`);
+});
