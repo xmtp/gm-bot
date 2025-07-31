9d025342913d08a67357f3276e417226617e9a8c
Fabri <fguespe@gmail.com>
2025-07-31 16:06:34 -0300

    Refactor message handling for improved clarity and flow (#53)
    * Refactor message handling for improved clarity and flow
    
    * Remove unnecessary message filtering for clarity
    
    * Filter out bot and non-text messages in stream processing

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { Client, DecodedMessage, LogLevel, type XmtpEnv } from "@xmtp/node-sdk";
+import { Client,  LogLevel, type XmtpEnv } from "@xmtp/node-sdk";
 import { createSigner, getDbPath, getEncryptionKeyFromHex, logAgentDetails, validateEnvironment } from "../helpers/client";
 
 const { WALLET_KEY, ENCRYPTION_KEY, XMTP_ENV ,LOGGING_LEVEL } = validateEnvironment([
@@ -26,33 +26,27 @@ async function main() {
   void logAgentDetails(client);
   console.log("Syncing conversations...");
   await client.conversations.sync();
-
   console.log("Waiting for messages...");
-  const onValue = async (message: DecodedMessage) => {
-    
-    
-
+  const stream = await client.conversations.streamAllMessages();
+  for await (const message of stream) {
     // Skip if the message is from the bot
     if(message.senderInboxId.toLowerCase() === client.inboxId.toLowerCase()) {
-      return;
+      continue;
     }
     // Skip if the message is not a text message
     if (message.contentType?.typeId !== "text") {
-      return;
+      continue;
     }
 
     console.log(`Received: ${message.content} from ${message.senderInboxId}`);
     const conversation = await client.conversations.getConversationById(message.conversationId)
-      if(!conversation) {
-        console.log(`Conversation not found: ${message.conversationId}`);
-        return;
-      }
-      await conversation.send("gm: " + message.content);
-      console.log(`${messageCount++} : ${message.content}`);
-     
-     
+    if(!conversation) {
+      console.log(`Conversation not found: ${message.conversationId}`);
+      continue;
+    }
+    await conversation.send("gm: " + message.content);
+    console.log(`${messageCount++} : ${message.content}`);
   }
-  client.conversations.streamAllMessages({onValue});
 }
 
 main().catch(console.error);
