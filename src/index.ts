659cbc1b743db958564d9c7d95c46d388b6dc91e
Fabrizio Guespe <fguespe@gmail.com>
2025-09-10 18:24:56 -0300

    Update bot and log messages

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,22 @@ import fs from "fs";
 // 2. Spin up the agent
 const agent = await Agent.create(createSigner(createUser()), {
   env: process.env.XMTP_ENV as "local" | "dev" | "production", // or 'production'
-  dbPath: getDbPath(`echo-bot`),
-  appVersion: "echo/1.0.0",
+  appVersion:'gm-bot/1.0.0',
+  dbPath: getDbPath("gm-bot-"+process.env.XMTP_ENV),
 });
 
 let count = 0;
 
 agent.on("text", async (ctx : any) => {
-  await ctx.conversation.send("gm: " + ctx.message.content);  
+  await ctx.conversation.send("gm: " + ctx.message.content);
   count++;
 });
 
 // 4. Log when we're ready
 agent.on("start", () => {
-  console.log(`We are online: ${getTestUrl(agent)}`);
+  console.log(`Waiting for messages...`);
+  console.log(`Address: ${agent.client.accountIdentifier?.identifier}`);
+  console.log(`🔗${getTestUrl(agent)}`);
 });
 
 await agent.start();
