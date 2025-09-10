0b43dc29d9ae42de69029c08cee19f3e5707c8e0
Fabrizio Guespe <fguespe@gmail.com>
2025-09-10 18:22:02 -0300

    Update

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,26 @@
 import "dotenv/config";
-import { Client,  LogLevel, type XmtpEnv } from "@xmtp/node-sdk";
-import { createSigner, getDbPath, getEncryptionKeyFromHex, logAgentDetails, validateEnvironment } from "../helpers/client";
 
-const { WALLET_KEY, ENCRYPTION_KEY, XMTP_ENV ,LOGGING_LEVEL } = validateEnvironment([
-  "WALLET_KEY",
-  "ENCRYPTION_KEY",
-  "XMTP_ENV",
-  "LOGGING_LEVEL",
-]);
+import { getDbPath } from "../helpers/client";
+import { Agent, createSigner, createUser, getTestUrl } from "@xmtp/agent-sdk";
 
-const signer = createSigner(WALLET_KEY as `0x${string}`);
-const dbEncryptionKey = getEncryptionKeyFromHex(ENCRYPTION_KEY);
-const env: XmtpEnv = (XMTP_ENV as XmtpEnv) || "dev";
+// 2. Spin up the agent
+const agent = await Agent.create(createSigner(createUser()), {
+  env: process.env.XMTP_ENV as "local" | "dev" | "production", // or 'production'
+  dbPath: getDbPath(`echo-bot`),
+  appVersion: "echo/1.0.0",
+});
 
-async function main() {
-  console.log(`Creating client on the '${env}' network...`);
-  
-  const client = await Client.create(signer, {
-    dbEncryptionKey,
-    env,
-    loggingLevel: LOGGING_LEVEL as LogLevel,
-    appVersion:'gm-bot/1.0.0',
-    dbPath: getDbPath("gm-bot-"+env),
-  });
-  let messageCount = 1;
-  void logAgentDetails(client);
-  console.log("Syncing conversations...");
-  console.time("syncAll");
-  await client.conversations.syncAll();
-  console.timeEnd("syncAll");
-  console.time("streamAllMessages");
-  const stream = await client.conversations.streamAllMessages();
-  console.timeEnd("streamAllMessages");
-  console.log("Waiting for messages...");
-  for await (const message of stream) {
-    // Skip if the message is from the bot
-    if(message.senderInboxId.toLowerCase() === client.inboxId.toLowerCase()) {
-      continue;
-    }
-    // Skip if the message is not a text message
-    if (message.contentType?.typeId !== "text") {
-      continue;
-    }
+let count = 0;
 
-    console.log(`Received: ${message.content} from ${message.senderInboxId}`);
-    const conversation = await client.conversations.getConversationById(message.conversationId)
-    if(!conversation) {
-      console.log(`Conversation not found: ${message.conversationId}`);
-      continue;
-    }
-    await conversation.send("gm: " + message.content);
-    console.log(`${messageCount++} : ${message.content}`);
-  }
-}
+agent.on("text", async (ctx : any) => {
+  await ctx.conversation.send("gm: " + ctx.message.content);  
+  count++;
+});
 
-main().catch(console.error);
+// 4. Log when we're ready
+agent.on("start", () => {
+  console.log(`We are online: ${getTestUrl(agent)}`);
+});
+
+await agent.start();
+
