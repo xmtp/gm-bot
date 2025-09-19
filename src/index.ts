1587d86aa6c7794e0d04ab22a50255d9bdf37aba
Fabri <fguespe@gmail.com>
2025-09-19 20:44:01 -0300

    Refactor logging in agent start (#73)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 
-import { Agent, getTestUrl, LogLevel, XmtpEnv  } from "@xmtp/agent-sdk";
-import fs from "fs";
+import { Agent, getTestUrl, logDetails  } from "@xmtp/agent-sdk";
 
 // Load .env file only in local development
 if (process.env.NODE_ENV !== 'production') process.loadEnvFile(".env");
@@ -8,7 +7,6 @@ if (process.env.NODE_ENV !== 'production') process.loadEnvFile(".env");
 
   // 2. Spin up the agent
 const agent = await Agent.createFromEnv({
-  env: process.env.XMTP_ENV as XmtpEnv,
   dbPath: (inboxId) =>
     process.env.RAILWAY_VOLUME_MOUNT_PATH ??
     "." + `/${process.env.XMTP_ENV}-${inboxId.slice(0, 8)}.db3`,
@@ -20,9 +18,7 @@ agent.on("text",  async (ctx: any) => {
 
 // 4. Log when we're ready
 agent.on("start", (): void => {
-  console.log(`Waiting for messages...`);
-  console.log(`Address: ${agent.client.accountIdentifier?.identifier}`);
-  console.log(`🔗${getTestUrl(agent)}`);
+  logDetails(agent.client)
 });
 
 await agent.start();
