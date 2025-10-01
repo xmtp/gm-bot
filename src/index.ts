e787ce8dfd456c9c4b1a34ed3be61553037363c7
fabri <fguespe@gmail.com>
2025-10-01 20:16:42 -0300

    Update agent-sdk and fix bugs

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
-import { Agent,  } from "@xmtp/agent-sdk";
-import { logDetails,  getTestUrl } from "@xmtp/agent-sdk/debug";
-
+import { Agent } from "@xmtp/agent-sdk";
+import { logDetails, getTestUrl } from "@xmtp/agent-sdk/debug";
 
 // Load .env file only in local development
-if (process.env.NODE_ENV !== 'production') process.loadEnvFile(".env");
-
+if (process.env.NODE_ENV !== "production") process.loadEnvFile(".env");
 
 const agent = await Agent.createFromEnv({
-  dbPath: (inboxId) =>(process.env.RAILWAY_VOLUME_MOUNT_PATH ?? "." )+ `/${process.env.XMTP_ENV}-${inboxId.slice(0, 8)}.db3`,
+  dbPath: (inboxId) =>
+    (process.env.RAILWAY_VOLUME_MOUNT_PATH ?? ".") +
+    `/${process.env.XMTP_ENV}-${inboxId.slice(0, 8)}.db3`,
 });
 
+agent.on("group", (ctx) => {
+  ctx.conversation.send("Hello everyone!");
+});
 
 agent.on("text", async (ctx) => {
   if (ctx.isDm()) {
@@ -24,7 +27,7 @@ agent.on("text", async (ctx) => {
   if (ctx.isGroup() && ctx.message.content.includes("@gm")) {
     const senderAddress = await ctx.getSenderAddress();
     console.log(
-      `Received message in group: ${ctx.message.content} by ${senderAddress}`,
+      `Received message in group: ${ctx.message.content} by ${senderAddress}`
     );
     await ctx.sendText("gm");
   }
@@ -40,9 +43,7 @@ agent.on("dm", async (ctx) => {
 
 // 4. Log when we're ready
 agent.on("start", (): void => {
-  logDetails(agent.client)
+  logDetails(agent.client);
 });
 
 await agent.start();
-
-
